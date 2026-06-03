3b603968074af7305d8d4c89da652f65adf07136
in-cotton <dlthals4187@gmail.com>
2026-06-03 21:50:09 +0900

    chore: 프로젝트 초기 디렉토리 구조 생성
    - chore: 프로젝트 초기 디렉토리 구조 생성
    - chore: 빈 폴더 유지를 위한 .gitkeep 파일 추가

---
+++ AppleGame.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+using OPP_project.Source.UI; .
+
+namespace AppleGame
+{
+
+    public static class AppleGame
+    {
+        // OLE 스레딩 모델을 Windows Forms에 맞게 설정하는 필수 보안 장치
+        [STAThread]
+
+        public static void Main()
+        {
+            // Windows Forms 환경을 초기화
+            ApplicationConfiguration.Initialize();
+
+            // 메인 메뉴 화면(MainMenuForm)을 화면에 띄우면서 게임을 시작!
+            Application.Run(new MainMenuForm());
+        }
+    }
+}
